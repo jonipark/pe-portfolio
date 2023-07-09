dd5b001e20cbfa647b89846e4917fd85396dbe36
Joeun Park <park49j@mtholyoke.edu>
2023-07-08 23:06:33 -0600

    redeploy-site script

---
+++ redeploy-site.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/pe-portfolio
+
+tmux kill-server
+
+git fetch && git reset origin/main --hard
+
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+
+tmux new-session -d -s pe-portfolio 'cd ~/pe-portfolio; source python3-virtualenv/bin/activate; flask run --host=0.0.0.0'
